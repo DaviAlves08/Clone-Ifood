475960eb07c26c858292dc4c0ce87c4443c8ae74
Davi Alves <139929297+DaviAlves08@users.noreply.github.com>
2024-02-12 19:23:13 -0300

    Update telacadastro.blade.php

--- resources/views/telacadastro.blade.php
+++ resources/views/telacadastro.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,23 +8,33 @@
     <title>Cadastro</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
+
 <body>
     <div class="backimg">
         <div class="backform">
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 36rem;">
                 <div class="card-body">
                     <h2 class="card-title">Cadastre-se Já!!!</h2>
                     <div class="m-3">
                         <form action="" method="">
                             <p>Faça seu cadastro abaixo!</p>
-                            <label for="usuario">Usuario</label>
-                            <input type="text" name="usuario">
-                            <label for="senha">Senha</label>
-                            <input type="password" name="senha">
-                            <input type="submit" value="Cadastrar">
+                            <div style="margin-bottom: 25px" class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="Usuario">
+                            </div>
+
+                            <div style="margin-bottom: 25px" class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                <input id="login-password" type="password" class="form-control" name="password" placeholder="Senha">
+                            </div>
+                            <div class="flex-row mt-5    ms-5">
+                                <input class="btn btn-primary me-3" type="submit" value="Cadastrar">
                         </form>
-                        <button type="button" class="btn btn-primary">
+                        <button type="button" class="btn btn-secondary ms-5">
                             <a href="index" class="text-decoration-none text-white">Voltar ao início</a>
                         </button>
                     </div>
@@ -31,5 +42,7 @@
             </div>
         </div>
     </div>
+    </div>
 </body>
+
 </html>
